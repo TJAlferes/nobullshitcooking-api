4b6841b045a7596a048427a3788d9b95ecb28074
T. J. Alferes <tjalferes@gmail.com>
2018-06-02 21:33:00 -0500

    down hill now

--- src/index.js
+++ src/index.js
@@ -213,6 +213,57 @@ app.delete('/ingredients/:id', async (req, res) => {
 });
 */
 
+// 6. list all ingredient types
+app.get('/ingredient-types', async (req, res) => {
+  try {
+    const sql = `SELECT ingredient_type_id, ingredient_type_name
+                 FROM nobsc_ingredient_types`;
+    const [ rows ] = await pool.execute(sql);
+
+    res.send(rows);
+
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+
+// 7. list specific ingredient type     (is this one needed?)
+app.get('/ingredient-types/:id', async (req, res) => {
+  try {
+    const id = req.params.id;  // sanitize and validate
+    const sql = `SELECT ingredient_type_id, ingredient_type_name
+                 FROM nobsc_ingredient_types
+                 WHERE ingredient_type_id = ?`;
+    const [ rows ] = await pool.execute(sql, [id]);
+  
+    res.send(rows);
+
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+
+//8. list all ingredients of specified type(s)     (this is the most important one)
+//     >>>>>     here, either make the route '/ingredients-by-type/:id' or read query
+//     >>>>>     add logic for determining and querying any give number types simultaeneously
+app.get('/ingredients-by-type/:id', async (req, res) => {
+  try {
+    const id = req.params.id;  // sanitize and validate
+    const sql = `SELECT ingredient_id, ingredient_name, ingredient_type_id, ingredient_image
+                 FROM nobsc_ingredients
+                 WHERE ingredient_type_id = ?`;
+    const [ rows ] = await pool.execute(sql, [id]);
+  
+    res.send(rows);
+
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+
 const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => console.log('Listening on port ' + PORT));
