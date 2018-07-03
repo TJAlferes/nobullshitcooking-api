30b4f379338f4cfc6adb4a9018c44a6a5ebe1fbd
T. J. Alferes <tjalferes@gmail.com>
2018-07-03 13:02:04 -0500

    working yay!

--- src/index.js
+++ src/index.js
@@ -19,14 +19,14 @@ const recipes = require('./routes/recipes');
 
 const app = express();
 const pool = mysql.createPool({
-  //host: process.env.DB_HOST,
-  //user: process.env.DB_USER,
-  //password: process.env.DB_PASSWORD,
-  //database: process.env.DB_DATABASE,
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  //host: process.env.RDS_HOSTNAME,
+  //user: process.env.RDS_USERNAME,
+  //password: process.env.RDS_PASSWORD,
+  //database: process.env.RDS_DB_NAME,
   waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS,
   connectionLimit: process.env.DB_CONNECTION_LIMIT,
   queueLimit: process.env.DB_QUEUE_LIMIT
--- src/routes/ingredients.js
+++ src/routes/ingredients.js
@@ -4,14 +4,14 @@ const router = express.Router();
 
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-  //host: process.env.DB_HOST,
-  //user: process.env.DB_USER,
-  //password: process.env.DB_PASSWORD,
-  //database: process.env.DB_DATABASE,
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  //host: process.env.RDS_HOSTNAME,
+  //user: process.env.RDS_USERNAME,
+  //password: process.env.RDS_PASSWORD,
+  //database: process.env.RDS_DB_NAME,
   waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS,
   connectionLimit: process.env.DB_CONNECTION_LIMIT,
   queueLimit: process.env.DB_QUEUE_LIMIT
@@ -104,7 +104,7 @@ router.delete('/:id', async (req, res) => {
 */
 
 // 6. list all ingredient types
-router.get('/types', async (req, res) => {
+router.get('/types/all', async (req, res) => {
   try {
     const sql = `SELECT ingredient_type_id, ingredient_type_name
                  FROM nobsc_ingredient_types`;
