c473e6c59800e49c612b7384ebe74fb96753693b
T. J. Alferes <tjalferes@gmail.com>
2018-08-10 04:28:35 -0500

    cleaning

--- src/routes/ingredients.js
+++ src/routes/ingredients.js
@@ -122,8 +122,8 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-/*
-// 3. submit new ingredient
+
+/*// 3. submit new ingredient
 router.post('/', async (req, res) => {
   try {
     const { id, name, typeId, image } = req.params;  // sanitize and validate
@@ -149,9 +149,7 @@ router.put('/:id', async (req, res) => {
                  FROM nobsc_ingredients
                  WHERE ingredient_id = ?`;
     const [ rows ] = await pool.execute(sql, [id]);
-
     res.send(rows);
-
   } catch(err) {
     console.log(err);
   }
@@ -167,14 +165,11 @@ router.delete('/:id', async (req, res) => {
                  WHERE ingredient_id = ?
                  LIMIT 1`;
     const [ rows ] = await pool.execute(sql, [id]);
-
     res.send(rows);
-
   } catch(err) {
     console.log(err);
   }
-});
-*/
+});*/
 
 // 6. list all ingredient types
 router.get('/types/all', async (req, res) => {
@@ -182,14 +177,11 @@ router.get('/types/all', async (req, res) => {
     const sql = `SELECT ingredient_type_id, ingredient_type_name
                  FROM nobsc_ingredient_types`;
     const [ rows ] = await pool.execute(sql);
-
     res.send(rows);
-
   } catch(err) {
     console.log(err);
   }
 });
-
 
 
 // 7. list specific ingredient type     (is this one needed?)
@@ -200,60 +192,10 @@ router.get('/types/:id', async (req, res) => {
                  FROM nobsc_ingredient_types
                  WHERE ingredient_type_id = ?`;
     const [ rows ] = await pool.execute(sql, [id]);
-  
     res.send(rows);
-
   } catch(err) {
     console.log(err);
   }
 });
 
-
-/*
-// 8. list all ingredients of specified type (user checks ONE type)
-router.get('/by-type/:id', async (req, res) => {
-  try {
-    const id = req.params.id;  // sanitize and validate
-    const sql = `SELECT ingredient_id, ingredient_name, ingredient_type_id, ingredient_image
-                 FROM nobsc_ingredients
-                 WHERE ingredient_type_id = ?`;
-    const [ rows ] = await pool.execute(sql, [id]);
-  
-    res.send(rows);
-
-  } catch(err) {
-    console.log(err);
-  }
-});
-
-
-
-// 9. list all ingredients of specified types (user checks multiple types)
-router.post('/of-types', async (req, res) => {
-  try {
-    const types = req.body.types;  // sanitize and validate
-
-    let ids = types.join(', '); // converts array to string for SQL query
-    console.log(ids);
-
-    let placeholders = '';
-    types.forEach(type => { // generate appropriate number of placeholders for SQL query
-      placeholders += '?,';
-    });
-    const placeholderString = placeholders.slice(0, -1);  // this just removes the comma at the end
-    console.log(placeholderString);
-    
-    const sql = `SELECT ingredient_id, ingredient_name, ingredient_type_id, ingredient_image
-                 FROM nobsc_ingredients
-                 WHERE ingredient_type_id = ${placeholderString}`;
-    const [ rows ] = await pool.execute(sql, [ids]);
-  
-    res.send(rows);
-    console.log(rows);
-    
-  } catch(err) {
-    console.log(err);
-  }
-});
-*/
 module.exports = router;
