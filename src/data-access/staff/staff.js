80e5c94b51f882ab481e95744f5adad04ce321bf
T. J. Alferes <tjalferes@gmail.com>
2019-03-09 22:50:53 -0600

    user and staff

--- src/data-access/staff/staff.js
+++ src/data-access/staff/staff.js
@@ -1,6 +1,62 @@
 class Staff {
   constructor(pool) {
     this.pool = pool;
+    this.viewAllStaff = this.viewAllStaff.bind(this);
+    this.viewStaffById = this.viewStaffById.bind(this);
+    this.createStaff = this.createStaff.bind(this);
+    this.updateStaff = this.updateStaff.bind(this);
+    this.deleteStaff = this.deleteStaff.bind(this);
+  }
+
+  viewAllStaff(starting, display) {
+    const sql = `
+      SELECT staffname, avatar
+      FROM nobsc_staff
+      ORDER BY staffname ASC
+      LIMIT ${starting}, ${display}
+    `;  // important (security): use ?, ? in LIMIT instead?
+    return pool.execute(sql);
+  }
+
+  viewStaffById(staffId) {
+    const sql = `
+      SELECT staffname, avatar
+      FROM nobsc_staff
+      WHERE staff_id = ?
+    `;
+    return pool.execute(sql, [staffId]);
+  }
+
+  createStaff(staffInfo) {
+    const { id, name, password, avatar } = staffInfo;
+    const sql = `
+      INSERT INTO nobsc_staff
+      (staff_id, staffname, password, avatar)
+      VALUES
+      (?, ?, ?, ?)
+    `;
+    return pool.execute(sql, [id, name, password, avatar]);
+  }
+
+  updateStaff(staffInfo) {
+    const { id, name, password, avatar } = staffInfo;
+    const sql = `
+      UPDATE nobsc_staff
+      SET staffname = ?, password = ?, avatar = ?
+      WHERE staff_id = ?
+      LIMIT 1
+    `;
+    return pool.execute(sql, [name, password, avatar, id]);
+  }
+
+  deleteStaff(staffId) {
+    const sql = `
+      DELETE
+      FROM nobsc_staff
+      WHERE staff_id = ?
+      LIMIT 1
+    `;
+    return pool.execute(sql, [staffId]);
   }
 }
 
--- src/data-access/user/user.js
+++ src/data-access/user/user.js
@@ -1,6 +1,62 @@
 class User {
   constructor(pool) {
     this.pool = pool;
+    this.viewAllUsers = this.viewAllUsers.bind(this);
+    this.viewUserById = this.viewUserById.bind(this);
+    this.createUser = this.createUser.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
+  }
+
+  viewAllUsers(starting, display) {
+    const sql = `
+      SELECT username, avatar
+      FROM nobsc_users
+      ORDER BY username ASC
+      LIMIT ${starting}, ${display}
+    `;  // important (security): use ?, ? in LIMIT instead?
+    return pool.execute(sql);
+  }
+
+  viewUserById(userId) {  // profile and plan info too (separate table?) (public or private option)
+    const sql = `
+      SELECT username, avatar
+      FROM nobsc_users
+      WHERE user_id = ?
+    `;
+    return pool.execute(sql, [userId]);
+  }
+
+  createUser(userInfo) {
+    const { id, name, password, avatar } = userInfo;
+    const sql = `
+      INSERT INTO nobsc_users
+      (user_id, username, password, avatar)
+      VALUES
+      (?, ?, ?, ?)
+    `;
+    return pool.execute(sql, [id, name, password, avatar]);
+  }
+
+  updateUser(userInfo) {
+    const { id, name, password, avatar } = userInfo;
+    const sql = `
+      UPDATE nobsc_users
+      SET username = ?, password = ?, avatar = ?
+      WHERE user_id = ?
+      LIMIT 1
+    `;
+    return pool.execute(sql, [name, password, avatar, id]);
+  }
+
+  deleteUser(userId) {
+    const sql = `
+      DELETE
+      FROM nobsc_users
+      WHERE user_id = ?
+      LIMIT 1
+    `;
+    return pool.execute(sql, [userId]);
   }
 }
 
