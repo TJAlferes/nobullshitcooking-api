a3e99352f5caaf68f5deed79cdae9b31744f5076
T. J. Alferes <tjalferes@gmail.com>
2019-06-23 10:38:21 -0500

    composing structs

---
+++ src/lib/validations/user/recipe.js
@@ -0,0 +1,51 @@
+const { struct } = require('superstruct');
+
+const validRequiredEquipment = struct({
+  key: 'number',
+  type: 'string',
+  amount: 'number',
+  equipment: 'string'
+});
+
+const validRequiredIngredient = struct({
+  key: 'number',
+  type: 'string',
+  unit: 'string',
+  amount: 'number',
+  ingredient: 'string'
+});
+
+const validRequiredSubrecipe = struct({
+  key: 'number',
+  cuisine: 'string',
+  type: 'string',
+  unit: 'string',
+  amount: 'number',
+  subrecipe: 'string'
+});
+
+const validRecipe = struct(
+  {
+    recipeId: 'number',
+    recipeTypeId: 'number',
+    cuisineId: 'number',
+    title: 'string',
+    description: 'string',
+    directions: 'string',
+    requiredEquipment: `[${validRequiredEquipment}]?`,
+    requiredIngredients: [validRequiredIngredient],
+    requiredSubrecipes: `[${validRequiredSubrecipe}]?`,
+    recipeImage: 'string',
+    equipmentImage: 'string',
+    ingredientsImage: 'string',
+    cookingImage: 'string'
+  },
+  {
+    recipeImage: 'nobsc-default-recipe-image',
+    equipmentImage: 'nobsc-default-equipment-image',
+    ingredientsImage: 'nobsc-default-ingredients-image',
+    cookingImage: 'nobsc-default-cooking-image'
+  }
+);
+
+module.exports = validRecipe;
