8dc663780a2dd0c6a7b04cd128d467fccae0d823
T. J. Alferes <tjalferes@gmail.com>
2019-08-26 17:34:48 -0500

    no type needed

--- src/controllers/user/get-signed-url/avatar.js
+++ src/controllers/user/get-signed-url/avatar.js
@@ -1,21 +1,13 @@
 require('dotenv').config();
-//const uuidv4 = require('uuid/v4');  // do in React instead?
 const AWS = require('aws-sdk');
-
-/*AWS.config.update({
-  region: 'us-east-1',
-  accessKeyId: process.env.AWS_NOBSC_USER_AVATARS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_NOBSC_USER_AVATARS_SECRET_ACCESS_KEY
-});*/
 
 const AWS_NOBSC_USER_AVATARS_S3_BUCKET = process.env.AWS_NOBSC_USER_AVATARS_S3_BUCKET;
 
 module.exports = async function(req, res) {
-  const fileName = `${req.session.userInfo.username}`;
+  const fileNameFullSize = `${req.session.userInfo.username}`;
+  const fileNameTinySize = `tiny-${req.session.userInfo.username}`;
   const fileType = req.sanitize(req.body.fileType);
-  console.log('avatar.type ', fileType);
 
-  //const s3 = new AWS.S3();
   const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_NOBSC_USER_AVATARS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_NOBSC_USER_AVATARS_SECRET_ACCESS_KEY
@@ -29,33 +21,25 @@ module.exports = async function(req, res) {
     });
   };
 
-  const signature = await getSignedUrlPromise('putObject', {
+  const signatureFullSize = await getSignedUrlPromise('putObject', {
     Bucket: AWS_NOBSC_USER_AVATARS_S3_BUCKET,
-    Key: fileName,
+    Key: fileNameFullSize,
     ContentType: fileType,
-    Expires: 50,
-    //ACL: 'public-read'  // ?
+    Expires: 50
   });
-  /*const signature = await s3.getSignedUrl(
-    'putObject',
-    {
-      Bucket: AWS_NOBSC_USER_AVATARS_S3_BUCKET,
-      Key: fileName,
-      ContentType: fileType,
-      Expires: 50
-    },
-    (err, url) => res.send({
-      success: true,
-      signedRequest: signature,
-      url: `https://${AWS_NOBSC_USER_AVATARS_S3_BUCKET}.s3.amazonaws.com/${fileName}`
-    })
-  );*/
 
-  console.log('signature ', signature);
+  const signatureTinySize = await getSignedUrlPromise('putObject', {
+    Bucket: AWS_NOBSC_USER_AVATARS_S3_BUCKET,
+    Key: fileNameTinySize,
+    ContentType: fileType,
+    Expires: 50
+  });
 
   res.json({
     success: true,
-    signedRequest: signature,
-    url: `https://${AWS_NOBSC_USER_AVATARS_S3_BUCKET}.s3.amazonaws.com/${fileName}${fileType}`
+    signedRequestFullSize: signatureFullSize,
+    urlFullSize: `https://${AWS_NOBSC_USER_AVATARS_S3_BUCKET}.s3.amazonaws.com/${fileNameFullSize}`,
+    signedRequestTinySize: signatureTinySize,
+    urlTinySize: `https://${AWS_NOBSC_USER_AVATARS_S3_BUCKET}.s3.amazonaws.com/${fileNameTinySize}`
   });
 };
