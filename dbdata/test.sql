60f83ac0c7fba878d6574b997cac9c4285024fb2
T. J. Alferes <tjalferes@gmail.com>
2019-09-10 12:35:47 -0500

    longer varchar

--- dbdata/test.sql
+++ dbdata/test.sql
@@ -142,7 +142,7 @@ CREATE TABLE `nobsc_recipe_methods` (
 CREATE TABLE `nobsc_friendships` (
   `user_id` int(10) unsigned NOT NULL,
   `friend_id` int(10) unsigned NOT NULL,
-  `status` varchar(8) NOT NULL,
+  `status` varchar(20) NOT NULL,
   FOREIGN KEY (`user_id`) REFERENCES `nobsc_users` (`user_id`),
   FOREIGN KEY (`friend_id`) REFERENCES `nobsc_users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
