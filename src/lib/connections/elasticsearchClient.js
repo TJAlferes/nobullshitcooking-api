fa65d2d2cc894f6416a30b231c7ffa67ba503cce
T. J. Alferes <tjalferes@gmail.com>
2019-10-25 18:57:36 -0500

    wo signing

--- src/lib/connections/elasticsearchClient.js
+++ src/lib/connections/elasticsearchClient.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const { Client, errors } = require('@elastic/elasticsearch');
-const {
+/*const {
   createAWSConnection,
   awsCredsifyAll,
   awsGetCredentials
-} = require('@acuris/aws-es-connection');
+} = require('@acuris/aws-es-connection');*/
 
 /*const esClient = (async function() {
   let makeClient;
@@ -28,23 +28,39 @@ const {
   return makeClient;  // () ?
 })();*/
 
-async function getAWSConnection() {
-  const awsCredentials = await awsGetCredentials();
+//import { createAWSConnector, awsCredsifyAll } from 'aws-es-connection';
+//import { Client } from '@elastic/elasticsearch';
+//import AWS from 'aws-sdk';
+
+/*export function createElasticsearchClient({ endpoint }) {
+  const AWSConnector = createAWSConnector(AWS.config.credentials);
+  return awsCredsifyAll(
+    new Client({
+      node: endpoint,
+      Connection: AWSConnector
+    })
+  )
+}*/
+
+/*async function getAWSConnection() {
+  const awsCredentials = await awsGetCredentials();  // or try the AWS.config.credentials
+  console.log('awsCredentials: ', awsCredentials);
   const AWSConnection = createAWSConnection(awsCredentials);
   return AWSConnection;
 }
 
 const AWSConnection = getAWSConnection();
 
+const createESClient = awsCredsifyAll(
+  new Client({
+    node: process.env.ELASTICSEARCH_PROD,
+    Connection: AWSConnection
+  })
+);
+console.log(createESClient);*/
+
 const esClient = process.env.NODE_ENV === 'production'
-? (
-  awsCredsifyAll(
-    new Client({
-      Connection: AWSConnection,
-      node: process.env.ELASTICSEARCH_PROD
-    })
-  )
-)
+? new Client({node: process.env.ELASTICSEARCH_PROD})
 : new Client({node: process.env.ES_DEV_NODE});
 
 /*const esClient = new Client({
