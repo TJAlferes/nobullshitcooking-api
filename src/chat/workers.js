f2de191385dc2d1054231df109d418b8ea08ec0a
T. J. Alferes <tjalferes@gmail.com>
2019-10-28 19:42:29 -0500

    hmm

--- src/chat/workers.js
+++ src/chat/workers.js
@@ -5,73 +5,90 @@ const { workerClient } = require('../lib/connections/redisConnection');
 const DELTA = 60 * 60 * 1000 * 2;  // 2 hours
 
 async function cleanUpRooms() {
-  await workerClient.zrangebyscore(
-    'rooms',
-    '-inf',
-    ((new Date).getTime() - DELTA),
-    function(err, rooms) {
-      if (err !== null) {
-        console.log(err);
-      } else {
-        rooms.forEach(function(room) {
-          workerClient
-          .multi()
-          .zrem('rooms', room)
-          .del(`rooms:${room}:chats`)
-          .exec();
+  try {
+    await workerClient.zrangebyscore(
+      'rooms',
+      '-inf',
+      ((new Date).getTime() - DELTA),
+      function(err, rooms) {
+        if (err !== null) {
+          console.log(err);
+        } else {
+          rooms.forEach(function(room) {
+            workerClient
+            .multi()
+            .zrem('rooms', room)
+            .del(`rooms:${room}:chats`)
+            .exec();
 
-          workerClient.zrangebyscore(
-            'users',
-            '-inf',
-            ((new Date).getTime() - DELTA),
-            function(err, users) {
-              if (err !== null) {
-                console.log(err);
-              } else {
-                users.forEach(function(user) {
-                  workerClient.zrem(`rooms:${room}`, user);
-                });
+            console.log('INSIDE! 1');
+            console.log('INSIDE! 1');
+
+            workerClient.zrangebyscore(
+              'users',
+              '-inf',
+              ((new Date).getTime() - DELTA),
+              function(err, users) {
+                if (err !== null) {
+                  console.log(err);
+                } else {
+                  console.log('INSIDE! 2');
+                  console.log('INSIDE! 2');
+                  users.forEach(function(user) {
+                    workerClient.zrem(`rooms:${room}`, user);
+                  });
+                }
               }
-            }
-          );
-        });
+            );
+          });
+        }
       }
-    }
-  );
+    );
+  } catch (err) {
+    console.log('=====!=====!=====!=====!=====', err);
+  }
 }
 
 async function cleanUpChats() {
-  await workerClient.zrange(
-    'rooms',
-    0,
-    -1,
-    function(err, rooms) {
-      workerClient.zrangebyscore(
-        'rooms',
-        '-inf',
-        ((new Date).getTime() - DELTA),
-        function(err, rooms) {
-          if (err !== null) {
-            console.log(err);
-          } else {
-            rooms.forEach(function(room) {
-              workerClient.zremrangebyscore(
-                `rooms:${room}:chats`,
-                '-inf',
-                ((new Date).getTime() - DELTA)
-              );
-            });
+  try {
+    await workerClient.zrange(
+      'rooms',
+      0,
+      -1,
+      function(err, rooms) {
+        workerClient.zrangebyscore(
+          'rooms',
+          '-inf',
+          ((new Date).getTime() - DELTA),
+          function(err, rooms) {
+            if (err !== null) {
+              console.log(err);
+            } else {
+              console.log('INSIDE! 3');
+              console.log('INSIDE! 3');
+              rooms.forEach(function(room) {
+                workerClient.zremrangebyscore(
+                  `rooms:${room}:chats`,
+                  '-inf',
+                  ((new Date).getTime() - DELTA)
+                );
+              });
+            }
           }
-        }
-      );
-    }
-  );
+        );
+      }
+    );
+  } catch (err) {
+    console.log('=====!=====!=====!=====!=====', err);
+  }
 }
 
 const cleanUp = async function() {
   console.log('Clean Up Isle NOBSC Messenger (START)');
+  console.log('Clean Up Isle NOBSC Messenger (START)');
   await cleanUpRooms();
   await cleanUpChats();
+  console.log('Clean Up Isle NOBSC Messenger (END)');
   console.log('Clean Up Isle NOBSC Messenger (END)');
 }
 
