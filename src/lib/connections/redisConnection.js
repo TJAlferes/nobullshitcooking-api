d10709af8fc0a3f03ca6e2bf1f9862cd5134ca83
T. J. Alferes <tjalferes@gmail.com>
2019-10-31 11:17:58 -0500

    primary

--- src/lib/connections/redisConnection.js
+++ src/lib/connections/redisConnection.js
@@ -1,27 +1,38 @@
 'use strict';
 
 const Redis = require('ioredis');
-
+/*
+const redisClusterOptions = [
+  {host: process.env.ELASTICACHE_PROD, port: 6379, password: process.env.REDIS_PASSWORD},
+  {host: process.env.ELASTICACHE_PROD, port: 6379, password: process.env.REDIS_PASSWORD}
+];
+const elasticacheWithTLS = {
+  dnsLookup: (address, callback) => callback(null, address),
+  redisOptions: {tls: {}}
+};
+const pubClient = new Redis.Cluster(redisClusterOptions, elasticacheWithTLS);
+const subClient = new Redis.Cluster(redisClusterOptions, elasticacheWithTLS);
+*/
 const pubClient = process.env.NODE_ENV === 'production'
-? new Redis({host: process.env.ELASTICACHE_PROD, port: 6379})
+? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
 : new Redis({host: 'redis-dev', port: 6379});
 
 const subClient = process.env.NODE_ENV === 'production'
-? new Redis({host: process.env.ELASTICACHE_PROD, port: 6379})
+? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
 : new Redis({host: 'redis-dev', port: 6379});
 
 const sessClient = process.env.NODE_ENV === 'production'
-? new Redis({host: process.env.ELASTICACHE_PROD, port: 6379})
+? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
 : new Redis({host: 'redis-dev', port: 6379});
 
 const workerClient = process.env.NODE_ENV === 'production'
-? new Redis({host: process.env.ELASTICACHE_PROD, port: 6379})
+? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
 : new Redis({host: 'redis-dev', port: 6379});
 
 //console.log('========== ioredis new Redis ========== ');
 //console.log('pubClient', pubClient);
 pubClient.on('connect', () => console.log('pub connect!!!!!!!!!!!'));
-//pubClient.on('ready', () => console.log('pub ready!!!!!!!!!!!'));
+pubClient.on('ready', () => console.log('pub ready!!!!!!!!!!!'));
 //pubClient.on('error', () => console.log('pub error!!!!!!!!!!!'));
 pubClient.on('close', () => console.log('pub close!!!!!!!!!!!'));
 //console.log('============================== ');
@@ -29,7 +40,7 @@ pubClient.on('close', () => console.log('pub close!!!!!!!!!!!'));
 //console.log('========== ioredis new Redis ========== ');
 //console.log('subClient', subClient);
 subClient.on('connect', () => console.log('sub connect!!!!!!!!!!!'));
-//subClient.on('ready', () => console.log('sub ready!!!!!!!!!!!'));
+subClient.on('ready', () => console.log('sub ready!!!!!!!!!!!'));
 //subClient.on('error', () => console.log('sub error!!!!!!!!!!!'));
 subClient.on('close', () => console.log('sub close!!!!!!!!!!!'));
 //console.log('============================== ');
@@ -37,7 +48,7 @@ subClient.on('close', () => console.log('sub close!!!!!!!!!!!'));
 //console.log('========== ioredis new Redis ========== ');
 //console.log('sessClient', sessClient);
 sessClient.on('connect', () => console.log('sess connect!!!!!!!!!!!'));
-//sessClient.on('ready', () => console.log('sess ready!!!!!!!!!!!'));
+sessClient.on('ready', () => console.log('sess ready!!!!!!!!!!!'));
 //sessClient.on('error', () => console.log('sess error!!!!!!!!!!!'));
 sessClient.on('close', () => console.log('sess close!!!!!!!!!!!'));
 //console.log('============================== ');
@@ -46,7 +57,7 @@ sessClient.on('close', () => console.log('sess close!!!!!!!!!!!'));
 //console.log('========== ioredis new Redis ========== ');
 //console.log('workerClient', workerClient);
 workerClient.on('connect', () => console.log('worker connect!!!!!!!!!!!'));
-//workerClient.on('ready', () => console.log('worker ready!!!!!!!!!!!'));
+workerClient.on('ready', () => console.log('worker ready!!!!!!!!!!!'));
 //workerClient.on('error', () => console.log('worker error!!!!!!!!!!!'));
 workerClient.on('close', () => console.log('worker close!!!!!!!!!!!'));
 //console.log('============================== ');
