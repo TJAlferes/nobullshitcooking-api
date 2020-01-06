e9ebcecd80e6af6ae913e89fccd1148577bef575
T. J. Alferes <tjalferes@gmail.com>
2020-01-06 12:04:22 -0600

    nginx

--- src/index.js
+++ src/index.js
@@ -228,7 +228,7 @@ if (app.get('env') === 'development') {
   });
 } else {
   app.use((error, req, res, next) => {
-    res.status(error.statusCode || 500).json({error: error.message});
+    res.status(error.statusCode || 500).json({error: error.message});  // ???
   });
 }
 
