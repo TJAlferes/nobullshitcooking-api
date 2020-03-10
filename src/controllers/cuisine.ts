6e8b0b0ffa6a886a6e3ee9beaea3508118618a8a
T. J. Alferes <tjalferes@gmail.com>
2020-03-09 22:13:00 -0500

    node upgrade

--- src/controllers/cuisine.ts
+++ src/controllers/cuisine.ts
@@ -1,14 +1,16 @@
+import { Request, Response } from 'express';
+
 const pool = require('../lib/connections/mysqlPoolConnection');
 const Cuisine = require('../mysql-access/Cuisine');
 const validCuisineRequest = require('../lib/validations/cuisine/cuisineRequest');
 
 const cuisineController = {
-  viewAllCuisines: async function(req, res) {
+  viewAllCuisines: async function(req: Request, res: Response) {
     const cuisine = new Cuisine(pool);
     const rows = await cuisine.viewAllCuisines();
     res.send(rows);
   },
-  viewCuisineById: async function(req, res) {
+  viewCuisineById: async function(req: Request, res: Response) {
     const cuisineId = Number(req.sanitize(req.params.cuisineId));
     validCuisineRequest({cuisineId});
     //if (cuisineId < 1 || cuisineId > 196) return res.send('invalid cuisine');
@@ -16,7 +18,7 @@ const cuisineController = {
     const [ row ] = await cuisine.viewCuisineById(cuisineId);
     res.send(row);
   },
-  viewCuisineDetailById: async function(req, res) {
+  viewCuisineDetailById: async function(req: Request, res: Response) {
     const cuisineId = Number(req.sanitize(req.params.cuisineId));
     validCuisineRequest({cuisineId});
     const cuisine = new Cuisine(pool);
