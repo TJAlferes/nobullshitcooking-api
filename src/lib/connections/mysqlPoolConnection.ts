7e0a5e84c72c7f5d49b0ee11117c2edb500b7543
T. J. Alferes <tjalferes@gmail.com>
2020-06-17 23:21:33 -0500

    integration

--- src/lib/connections/mysqlPoolConnection.ts
+++ src/lib/connections/mysqlPoolConnection.ts
@@ -1,27 +1,37 @@
+'use strict';
+
 import mysql from 'mysql2/promise';
 
 export const pool = process.env.NODE_ENV === 'production'
-? mysql.createPool({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DB_NAME,
-  waitForConnections: Boolean(process.env.DB_WAIT_FOR_CONNECTIONS),
-  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT),
-  queueLimit: Number(process.env.DB_QUEUE_LIMIT)
-})
-: mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: Boolean(process.env.DB_WAIT_FOR_CONNECTIONS),
-  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT),
-  queueLimit: Number(process.env.DB_QUEUE_LIMIT),
-  insecureAuth: true
-});
+  ? mysql.createPool({
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    waitForConnections: Boolean(process.env.DB_WAIT_FOR_CONNECTIONS),
+    connectionLimit: Number(process.env.DB_CONNECTION_LIMIT),
+    queueLimit: Number(process.env.DB_QUEUE_LIMIT)
+  })
+  : process.env.NODE_ENV === 'test'
+    ? mysql.createPool({
+      host: process.env.TEST_MYSQL_HOST,
+      user: process.env.TEST_MYSQL_USER,
+      password: process.env.TEST_MYSQL_PASSWORD,
+      database: process.env.TEST_MYSQL_DATABASE,
+      waitForConnections: true,
+      connectionLimit: 50,
+      queueLimit: 0,
+      insecureAuth: true
+    })
+    : mysql.createPool({
+      host: process.env.DEV_MYSQL_HOST,
+      user: process.env.DEV_MYSQL_USER,
+      password: process.env.DEV_MYSQL_PASSWORD,
+      database: process.env.DEV_MYSQL_DATABASE,
+      waitForConnections: true,
+      connectionLimit: 50,
+      queueLimit: 0,
+      insecureAuth: true
+    });
 
-//console.log('========== mysql2/promise createPool ========== ');
-//console.log('pool', pool);
-//console.log('============================== ');
 // set up proper retry logic
--- src/lib/connections/redisConnection.ts
+++ src/lib/connections/redisConnection.ts
@@ -3,39 +3,47 @@
 import Redis from 'ioredis';
 
 export const pubClient = process.env.NODE_ENV === 'production'
-? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
-: new Redis({host: 'redis-dev', port: 6379});
+  ? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
+  : process.env.NODE_ENV === 'test'
+    ? new Redis({host: 'redis-test', port: 6379})
+    : new Redis({host: 'redis-dev', port: 6379});
 
 export const subClient = process.env.NODE_ENV === 'production'
-? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
-: new Redis({host: 'redis-dev', port: 6379});
+  ? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
+  : process.env.NODE_ENV === 'test'
+    ? new Redis({host: 'redis-test', port: 6379})
+    : new Redis({host: 'redis-dev', port: 6379});
 
 export const sessClient = process.env.NODE_ENV === 'production'
-? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
-: new Redis({host: 'redis-dev', port: 6379});
+  ? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
+  : process.env.NODE_ENV === 'test'
+    ? new Redis({host: 'redis-test', port: 6379})
+    : new Redis({host: 'redis-dev', port: 6379});
 
 export const workerClient = process.env.NODE_ENV === 'production'
-? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
-: new Redis({host: 'redis-dev', port: 6379});
+  ? new Redis({host: process.env.ELASTICACHE_PROD_PRIMARY, port: 6379})
+  : process.env.NODE_ENV === 'test'
+    ? new Redis({host: 'redis-test', port: 6379})
+    : new Redis({host: 'redis-dev', port: 6379});
 
 // set up proper retry logic
 
-pubClient.on('connect', () => console.log('pub connect!'));
-pubClient.on('ready', () => console.log('pub ready!'));
-pubClient.on('error', () => console.log('pub error!'));
-pubClient.on('close', () => console.log('pub close!'));
+pubClient.on('connect', () => console.log('pubClient connected'));
+pubClient.on('ready', () => console.log('pubClient ready'));
+pubClient.on('error', () => console.log('pubClient error'));
+pubClient.on('close', () => console.log('pubClient closed'));
 
-subClient.on('connect', () => console.log('sub connect!'));
-subClient.on('ready', () => console.log('sub ready!'));
-subClient.on('error', () => console.log('sub error!'));
-subClient.on('close', () => console.log('sub close!'));
+subClient.on('connect', () => console.log('subClient connected'));
+subClient.on('ready', () => console.log('subClient ready'));
+subClient.on('error', () => console.log('subClient error'));
+subClient.on('close', () => console.log('subClient closed'));
 
-sessClient.on('connect', () => console.log('sess connect!'));
-sessClient.on('ready', () => console.log('sess ready!'));
-sessClient.on('error', () => console.log('sess error!'));
-sessClient.on('close', () => console.log('sess close!'));
+sessClient.on('connect', () => console.log('sessClient connected'));
+sessClient.on('ready', () => console.log('sessClient ready'));
+sessClient.on('error', () => console.log('sessClient error'));
+sessClient.on('close', () => console.log('sessClient closed'));
 
-workerClient.on('connect', () => console.log('worker connect!'));
-workerClient.on('ready', () => console.log('worker ready!'));
-workerClient.on('error', () => console.log('worker error!'));
-workerClient.on('close', () => console.log('worker close!'));
+workerClient.on('connect', () => console.log('workerClient connected'));
+workerClient.on('ready', () => console.log('workerClient ready'));
+workerClient.on('error', () => console.log('workerClient error'));
+workerClient.on('close', () => console.log('workerClient closed'));
