ddeb442245c7da6b22fc795a42001eb9c0b858c3
T. J. Alferes <tjalferes@gmail.com>
2020-07-04 17:56:17 -0500

    tests config update

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,8 @@ if (app.get('env') === 'production') {
   PORT = Number(process.env.PORT) || 8081;
   server
   .listen(PORT, '127.0.0.1', () => console.log('Listening on port ' + PORT));
-} else if (app.get('env') === 'test') {
-  PORT = Number(process.env.TEST_PORT) || 3004;
-  server
-  .listen(PORT, '0.0.0.0', () => console.log('Listening on port ' + PORT));
-} else if (app.get('env') === 'development') {
-  PORT = Number(process.env.DEV_PORT) || 3003;
+} else {
+  PORT = Number(process.env.PORT) || 3003;
   server
   .listen(PORT, '0.0.0.0', () => console.log('Listening on port ' + PORT));
 }
--- src/lib/connections/elasticsearchClient.ts
+++ src/lib/connections/elasticsearchClient.ts
@@ -4,6 +4,4 @@ import { Client } from '@elastic/elasticsearch';
 
 export const esClient = process.env.NODE_ENV === 'production'
 ? new Client({node: process.env.ELASTICSEARCH_PROD})
-: process.env.NODE_ENV === 'test'
-  ? new Client({node: "http://192.168.99.100:9200"})
-  : new Client({node: "http://192.168.99.100:9200"});
+: new Client({node: "http://192.168.99.100:9200"});
