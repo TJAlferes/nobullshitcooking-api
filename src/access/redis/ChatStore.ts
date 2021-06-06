d4da35a754be5750f2d9ec8585416bcac92609d6
T. J. Alferes <tjalferes@gmail.com>
2021-06-05 19:21:36 -0500

    good spot

--- src/access/redis/ChatStore.ts
+++ src/access/redis/ChatStore.ts
@@ -2,29 +2,22 @@ import { Redis } from 'ioredis';
 
 import { IChatUser, IMessage } from '../../chat/entities/types';
 
-const TTL = 24 * 60 * 60;
-
 export class ChatStore implements IChatStore {
   client: Redis;
 
   constructor(client: Redis) {
     this.client = client;
-    this.createRoom = this.createRoom.bind(this);
     this.getUsersInRoom = this.getUsersInRoom.bind(this);
+    this.getUserSocketId = this.getUserSocketId.bind(this);
+    //this.getRooms
+    //this.getMessages
+    this.createUser = this.createUser.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+    this.createMessage = this.createMessage.bind(this);
     this.addUserToRoom = this.addUserToRoom.bind(this);
     this.removeUserFromRoom = this.removeUserFromRoom.bind(this);
-    this.getUserSocketId = this.getUserSocketId.bind(this);
-    this.createUser = this.createUser.bind(this);
     this.deleteUser = this.deleteUser.bind(this);
-    this.createMessage = this.createMessage.bind(this);
   }
-
-  // not needed?  you'd still need part of the worker...
-  async createRoom(room: string) {
-    if (room !== '') await this.client.zadd('rooms', `${Date.now()}`, room);
-  }
-
-  //async getMessages(room: string) {}
 
   async getUsersInRoom(room: string) {
     const ids = await this.client.zrange(`rooms:${room}:users`, 0, -1);
@@ -37,65 +30,71 @@ export class ChatStore implements IChatStore {
     }
     return users;
   }
+
+  async getUserSocketId(id: number) {
+    const socketId = await this.client.hget(`user:${id}`, 'socketId');
+    return socketId;
+  }
+
+  //async getRooms(cb) {}
+
+  //async getMessages(room: string, cb) {}
+
+  async createUser({ id, username, sessionId, socketId }: ICreatingChatUser) {
+    await this.client.multi()
+      .hset(
+        `user:${id}`,
+        'username', username, 'sessionId', sessionId, 'socketId', socketId
+      )
+      .zadd('users', Date.now(), `${id}`)
+      .exec();
+  }
+
+  async createRoom(room: string) {  // not needed?
+    if (room !== '') await this.client.zadd('rooms', Date.now(), room);
+  }
+
+  async createMessage(message: IMessage) {
+    await this.client.multi()
+      .zadd(`rooms:${message.to}:messages`, JSON.stringify(message))
+      .zadd('users', Date.now(), `${message.from.id}`)
+      .zadd('rooms', Date.now(), `${message.to}`)
+      .exec();
+  }
   
   async addUserToRoom(id: number, room: string) {
-    await this.client
-      .multi()
-      .zadd(`rooms:${room}:users`, `${Date.now()}`, `${id}`)
+    await this.client.multi()
+      .zadd(`rooms:${room}:users`, Date.now(), `${id}`)
+      .zadd('users', Date.now(), `${id}`)
+      .zadd('rooms', Date.now(), room)
       .set(`user:${id}:room`, room)
       .exec();
   }
   
   async removeUserFromRoom(id: number, room: string) {
-    await this.client
-      .multi()
-      .zrem(`rooms:${room}:users`, id)
+    await this.client.multi()
+      .zrem(`rooms:${room}:users`, `${id}`)
       .del(`user:${id}:room`)
-      .exec();
-  }
-
-  async getUserSocketId(id: number) {
-    const socketId = await this.client.hget(`user:${id}`, 'socketid');
-    return socketId;
-  }
-
-  async createUser({ id, username, sessionId, socketId }: ICreatingChatUser) {
-    await this.client
-      .multi()
-      .hset(
-        `user:${id}`,
-        'username', username, 'sessionId', sessionId, 'socketId', socketId
-      )
-      //.zadd('users', `${Date.now()}`, `${id}`)
-      .expire(`user:${id}`, TTL)
-      //.expire('users')
       .exec();
   }
 
   async deleteUser(id: number) {
     await this.client.del(`user:${id}`);
   }
-
-  async createMessage(message: IMessage) {
-    await this.client
-      .multi()
-      .zadd(`rooms:${message.to}:messages`, JSON.stringify(message))
-      .expire(`rooms:${message.to}:messages`, TTL)
-      //.zadd('rooms', `${(new Date).getTime()}`, JSON.stringify(chat.room))
-      .exec();
-  }
 }
 
 export interface IChatStore {
   client: Redis;
-  createRoom(room: string): void;
   getUsersInRoom(room: string): Promise<IChatUser[]>;
+  getUserSocketId(id: number): Promise<string | null>;
+  //getRooms
+  //getMessages
+  createUser(userInfo: ICreatingChatUser): void;
+  createRoom(room: string): void;
+  createMessage(message: IMessage): void;
   addUserToRoom(id: number, room: string): void;
   removeUserFromRoom(id: number, room: string): void;
-  getUserSocketId(id: number): Promise<string | null>;
-  createUser(userInfo: ICreatingChatUser): void;
   deleteUser(id: number): void;
-  createMessage(message: IMessage): void;
 }
 
 interface ICreatingChatUser {
--- src/chat/workers.ts
+++ src/chat/workers.ts
@@ -2,79 +2,53 @@
 
 import { Redis } from 'ioredis';
 
-const DELTA = 60 * 60 * 1000 * 2;  // 2 hours
+const INTERVAL = 60 * 60 * 1000 * 2; // 7200000 milliseconds = 2 hours
+const DELTA = 60 * 60 * 1000 * 3;  // 10800000 milliseconds = 3 hours
+const max = Date.now() - DELTA;
 
-async function cleanUpRooms(workerClient: Redis) {
-  await workerClient.zrangebyscore(
-    'rooms',
-    '-inf',
-    ((new Date).getTime() - DELTA),
-    function(err, rooms) {
-      if (err !== null) {
-        console.log(err);
-        return;
-      }
+function cleanUpRooms(client: Redis) {
+  client.zrangebyscore('rooms', '-inf', max, (err, rooms) => {
+    if (err !== null) {
+      console.log(err);
+      return;
+    }
 
-      rooms.forEach(function(room) {
-        workerClient
-        .multi()
+    rooms.forEach(room => {
+      client.multi()
         .zrem('rooms', room)
+        .del(`rooms:${room}:users`)
         .del(`rooms:${room}:messages`)
         .exec();
-
-        workerClient.zrangebyscore(
-          'users',
-          '-inf',
-          ((new Date).getTime() - DELTA),
-          function(err, users) {
-            if (err !== null) {
-              console.log(err);
-              return;
-            }
-
-            users.forEach(function(user) {
-              workerClient.zrem(`rooms:${room}`, user);
-            });
-          }
-        );
-      });
-    }
-  );
+    });
+  });
 }
 
-// rooms?
-async function cleanUpChats(workerClient: Redis) {
-  await workerClient.zrange(
-    'rooms',
-    0,
-    -1,
-    function(err, rooms) {
-      workerClient.zrangebyscore(
-        'rooms',
-        '-inf',
-        ((new Date).getTime() - DELTA),
-        function(err, rooms) {
-          if (err !== null) {
-            console.log(err);
-            return;
-          }
-
-          rooms.forEach(function(room) {
-            workerClient.zremrangebyscore(
-              `rooms:${room}:messages`,
-              '-inf',
-              ((new Date).getTime() - DELTA)
-            );
-          });
-        }
-      );
+function cleanUpUsers(client: Redis) {
+  client.zrangebyscore('users', '-inf', max, (err, users) => {
+    if (err !== null) {
+      console.log(err);
+      return;
     }
-  );
+
+    users.forEach(id => {
+      client.multi()
+        .zrem('users', id)
+        .del(`user:${id}`)
+        .del(`user:${id}:room`)
+        .exec();
+    });
+  });
 }
 
-export async function cleanUp(workerClient: Redis) {
-  console.log('Clean Up Isle NOBSC Messenger (START)');
-  await cleanUpRooms(workerClient);
-  await cleanUpChats(workerClient);
-  console.log('Clean Up Isle NOBSC Messenger (END)');
+// blocks? make async?
+export function chatCleanUp(client: Redis) {
+  function cleanUp() {
+    console.log('START Chat Clean Up');
+    cleanUpRooms(client);
+    cleanUpUsers(client);
+    console.log('END Chat Clean Up');
+  }
+
+  setInterval(cleanUp, INTERVAL);
+  cleanUp();
 }
