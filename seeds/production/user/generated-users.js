fcd2bc64d83d3634166a09b725673e447eb5ef57
T. J. Alferes <tjalferes@tjalferes.com>
2023-10-30 00:42:08 -0500

    village

--- seeds/production/user/generated-users.js
+++ seeds/production/user/generated-users.js
@@ -1,16 +1,44 @@
 export const users = [
   {
     "user_id": "00000000-0000-0000-0000-000000000000",
-    "email": "nobsc_unknown@gmail.com",
-    "password": "$2b$10$aJIGanbHsHigkWiQrMkQFeIwKAaw6CFTG7Ddh.iwkcjqNYh38igvS",
+    "email": "nobscunknown@gmail.com",
+    "password": "$2b$10$mCnVx4gl5uBuR6xEsh4jn.FHbzpfC51KfZ8i5Hpsf0qz4DAaDUJaK",
     "username": "Unknown",
     "confirmation_code": null
   },
   {
     "user_id": "11111111-1111-1111-1111-111111111111",
     "email": "nobsc@nobullshitcooking.com",
-    "password": "$2b$10$G59VJPtVn7iwT78RHWbW3.hsnVnioVfMep49G9qJ5WC1P5Z67BWUi",
+    "password": "$2b$10$6DOmjLpFF.ZOGq0jUNG/e.cEM/RuJyaCXtkgh2PdWjlOHL/eVXkEC",
     "username": "NOBSC",
     "confirmation_code": null
+  },
+  {
+    "user_id": "22222222-2222-2222-2222-222222222222",
+    "email": "nobsctester@gmail.com",
+    "password": "$2b$10$WaZUh48yYmaeeVRvr0il5uZp474eK.sMXF7hlX.aEBw5Azit20/aG",
+    "username": "Tester",
+    "confirmation_code": null
+  },
+  {
+    "user_id": "33333333-3333-3333-3333-333333333333",
+    "email": "fakeuser1@gmail.com",
+    "password": "$2b$10$oGmnjkS8MaCu7BFU3E7k/e5yQhN02.o8xI8iTKNiBeapmp3yyMIJq",
+    "username": "FakeUser1",
+    "confirmation_code": null
+  },
+  {
+    "user_id": "44444444-4444-4444-4444-444444444444",
+    "email": "fakeuser2@gmail.com",
+    "password": "$2b$10$NcvU/D1nUIHxYvTxrF7MfudPb4EmW/6xZ2GXY59W6g9gX0MCeQ..y",
+    "username": "FakeUser2",
+    "confirmation_code": null
+  },
+  {
+    "user_id": "55555555-5555-5555-5555-555555555555",
+    "email": "fakeunconfirmeduser1@gmail.com",
+    "password": "$2b$10$4D/PF7S3Cjx4U8ZtFTGxZOEtolD5DhmsgwAcI3Z/MzFMNPuDFWeFK",
+    "username": "FakeUnconfirmedUser1",
+    "confirmation_code": "01010101-0101-0101-0101-010101010101"
   }
 ];
