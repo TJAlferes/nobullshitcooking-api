6d2666c6c49816b12b670e3a6e8e76dc79a0a2c3
T. J. Alferes <tjalferes@gmail.com>
2024-01-17 00:48:30 -0600

    certbot shit

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d nobsc-api-staging.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email tjalferes@tjalferes.com
+sudo certbot -n -d nobsc-api-1.com --nginx --agree-tos --email tjalferes@tjalferes.com
